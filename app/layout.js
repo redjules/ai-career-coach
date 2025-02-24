413c4415114ccd18ebaa576f02b7836e20d03e46
Ana <anapedrabcn@gmail.com>
2025-02-24 17:55:31 +0100

    final app

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,29 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sensai - AI Career Coach",
+  title: "AI Career Coach",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
           <ThemeProvider
             attribute="class"
@@ -22,14 +31,13 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
-            {/* header */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-            {/*  footer */}
-            <footer className="bg-muted/50 py-20">
+            <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 🩷 by Ana Stone</p>
+                <p>Made with 💗 by Ana Stone</p>
               </div>
             </footer>
           </ThemeProvider>
